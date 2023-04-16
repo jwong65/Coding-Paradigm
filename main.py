498f2931dcf5b37575dc0f05c2a47591b81c688b
jwong65 <jason.wong6507@gmail.com>
2023-04-16 00:21:40 -0400

    Anakin Pod which inherits from Podracer

--- main.py
+++ main.py
@@ -17,7 +17,19 @@ class Podracer:
         self.max_speed = max_speed
         self.condition = condition
         self.price = price
+    
+    def repair(self):
+        self.condition = 'repaired'
         
 # Define a repair() method that will update the condition of the podracer to "repaired".
 # Define a new class, AnakinsPod that inherits the Podracer class, but also contains a special method called boost that will multiply max_speed by 2.
+
+class AnakinsPod(Podracer):
+    def __init__(self, max_speed, condition, price):
+        super().__init__(max_speed, condition, price)
+    
+    def boost(self):
+        self.max_speed = self.max_speed*2
+
+
 # Define another class that inherits Podracer and call this one SebulbasPod. This class should have a special method called flame_jet that will update the condition of another podracer to "trashed".
